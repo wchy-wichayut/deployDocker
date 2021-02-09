4defc6ba1a09e862098d2aee02112ea9ec14c752
Wichayut Ketsith <wichayut.ks@gmail.com>
2021-02-09 17:16:00 +0700

    ...

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-app =FastAPI()
-app.mount('/static',StaticFiles(directory='static'), name='static')
+app = FastAPI()
+app.mount('/static', StaticFiles(directory='static'), name='static')
 templates = Jinja2Templates(directory='templates')
+
 
 @app.get('/')
 def index(request: Request):
